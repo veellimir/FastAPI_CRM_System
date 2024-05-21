6f56ecfb5b956e835dbcf7c6d227ed67f3e9a508
vellimir <kkazancov@gmail.com>
2024-05-21 23:01:56 +0400

    port port for cors

--- app/main.py
+++ app/main.py
@@ -17,9 +17,9 @@ origins = [
     "localhost:9000",
     "https://crm-sistem-daniilkuns-projects.vercel.app",
     "https://crm-sistem-daniilkuns-projects.vercel.app/auth/login",
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:5175",
+    "http://localhost:5173/",
+    "http://localhost:5174/",
+    "http://localhost:5175/",
 ]
 
 app.add_middleware(
