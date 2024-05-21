626ab916b4f6652a02d11e855f1e08555364f0bc
vellimir <117735531+veellimir@users.noreply.github.com>
2024-05-21 23:33:59 +0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
-    allow_headers=["Content-type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Authorization", "Authorization"]
+    allow_headers=["Content-type", "Set-Cookie", "Access-Control-Allow-Headers", "Authorization"]
 )
 
 app.include_router(auth_router)
